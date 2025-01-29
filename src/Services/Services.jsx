989acf90c56148cba5ffa2a5614aad67575db822
Preeti-agan <vijayagan92@gmail.com>
2025-01-29 14:23:59 +0530

    last commit

--- src/Services/Services.jsx
+++ src/Services/Services.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './Services.css'
 const Services = () => {
   return (
+    <div className='sep'>
     <div className='services'>
       <div className='services1'>
         <img  className="ser-img"src="https://templatemo.com/templates/templatemo_578_first_portfolio/images/handshake-man-woman-after-signing-business-contract-closeup.jpg" alt=""></img>
         <h2 className='ser-h2'>Services</h2>
       </div>
+    </div>
     </div>
    
   )
